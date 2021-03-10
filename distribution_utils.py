b2f904fef064b9b936b9c96942b249d5906fd95c
Ashilov2 <Andrey.Shilov2@harman.com>
2021-03-11 02:45:46 +0300

    added new ra and gaussian dist

--- distribution_utils.py
+++ distribution_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy
 import scipy.stats
+import scipy.integrate
+import math
 
 
 def create_binomial_costs_with_binomial_T(m, d=10, T_min=10, T_max=100, verbose=False):
@@ -40,10 +42,42 @@ def create_binomial_costs(m, d=10, T_min=10, T_max=100, verbose=False):
     return c_hat, c_bar, T, p
 
 
+def create_normal_costs(m, d=10, T_min=10, T_max=100, std=2, verbose=False):
+    def gaussian(x):
+        return math.exp(- ((x - mean) ** 2) / (2 * (std ** 2))) / (math.sqrt(2 * math.pi) * std)
+
+    def count_expectation():
+        p = np.zeros(d)
+        p[0] = scipy.integrate.quad(gaussian, -np.inf, 1.5)[0]
+        expectation = 1 * p[0]
+        for i in range(1, d - 1):
+            p[i] = scipy.integrate.quad(gaussian, i+0.5, i+1.5)[0]
+            expectation += (i + 1) * p[i]
+        p[d-1] = scipy.integrate.quad(gaussian, d-0.5, np.inf)[0]
+        expectation += d * p[d-1]
+        assert abs(p.sum() - 1) < 1e-3
+        return expectation
+    T = np.random.randint(T_min, T_max + 1, size=m)  # uniform T
+    c_hat = []  # incomplete data for every arc
+    c_bar = np.zeros(m)
+    for a in range(len(T)):
+        mean = np.random.randint(1, d)
+        normal_values = np.random.randn(T[a]) * std + mean
+        normal_values[normal_values <= 0.5] = 1
+        normal_values[normal_values >= d + 0.5] = d
+        normal_values = np.round(normal_values, 0).astype(int)
+        c_hat.append(normal_values)
+        c_bar[a] = count_expectation()
+    if verbose:
+        print("Generated distribution")
+        print("Check mean estimation:", np.mean(c_hat[0]), c_bar[0])
+    return c_hat, c_bar, T, None
+
+
 def generate_binomial_samples(T, p, d, verbose=False):
     c_hat = []  # incomplete data for every arc
     for a in range(len(T)):
-        c_hat.append(scipy.stats.binom.rvs(n=d-1, p=p[a], size=T[a]) + 1)    
+        c_hat.append(scipy.stats.binom.rvs(n=d-1, p=p[a], size=T[a]) + 1)
     c_bar = (d - 1)*p + 1  # nominal mean
     if verbose:
         print("Generated distribution")
--- main.py
+++ main.py
@@ -78,7 +78,74 @@ def solve_cvx_dual_individual(q_hat_a, r_a):
     return prob.solution.opt_val
 
 
-def get_c_worst_DRO(q_hat, alpha, T):
+def count_classic_cnk_ra(T_a, alpha, d, m):
+    return -1 / T_a * math.log(alpha / (m * Cnk(T[a] + d - 1, d - 1)))
+
+
+def count_agrawal_ra(T_a, alpha, d, m):
+    """
+    Theorem 1.2 from https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=9097208
+    """
+    # is always epsilon > (k-1)/n ?
+    k = d  # paper notation
+    n = T_a  # paper notation
+
+    right_part = alpha / m
+    left_part_mulitpier = math.pow(math.e * T_a / (d - 1), d - 1)
+    def objective_function(ra):
+        """
+        Solve transcendent equation by minimizing difference between left and right part
+        """
+        return abs(left_part_mulitpier * math.pow(ra, d-1) * math.exp(-ra * T_a) - right_part)
+    min_value = scipy.optimize.minimize(objective_function, x0=d + 1, method='Nelder-Mead')
+    ra = min_value['x'][0]
+    return ra
+
+def count_mardia_ra(T_a, alpha, d, m):
+    """
+    Constraint on Kullback-Leibler distance described in
+    https://pdfs.semanticscholar.org/70aa/1ca0395b6282f2183ea9889583ba25352748.pdf
+    """
+    def get_cj(j):
+        if j == 0:
+            return math.pi
+        elif j == 1:
+            return 2
+        elif j % 2 == 0:
+            nomirator = np.prod(np.arange(1, j, 2)) * math.pi
+            denominator = np.prod(np.arange(2, j + 1, 2))
+        else:
+            nomirator = np.prod(np.arange(2, j, 2)) * 2
+            denominator = np.prod(np.arange(1, j, 2))
+        assert nomirator > 0 and denominator > 0, f"Value overflow processing c_{j}! Please decrease d"
+        return nomirator / denominator
+
+    def get_km(m):
+        """
+        Equality (14) of paper
+        """
+        if m % 2 == 0:
+            nomirator = math.pi * math.pow(2 * math.pi, m//2)
+            denominator = np.prod(np.arange(2, m + 1, 2))
+        else:
+            nomirator = math.pow(2 * math.pi, (m+1)//2)
+            denominator = np.prod(np.arange(1, m + 1, 2))
+        assert nomirator > 0 and denominator > 0, f"Value overflow processing k_{m}! Please decrease d"
+        return nomirator / denominator
+
+    # is it OK that estimation is not strictly e^(-T_a r_a), but sqrt(T_a) * e^(-T_a r_a) ?
+    square_bracket = 0  # equation (15)
+    k = d  # paper notation
+    K_i_minus1 = 1
+    for i in range(k - 1):
+        square_bracket += (K_i_minus1 * math.pow(math.e * math.sqrt(T_a) / (2 * math.pi), i))
+        K_i_minus1 *= get_cj(i)
+    square_bracket *= (3 * get_cj(1) / get_cj(2))
+    ra = - 1 / T_a * math.log(alpha / (m * square_bracket))
+    return ra
+
+
+def get_c_worst_DRO(q_hat, alpha, T, ra_choice):
     """
     Solve with different radiuses
     """
@@ -99,7 +166,12 @@ def get_c_worst_DRO(q_hat, alpha, T):
 
     c_worst = []
     for a in range(m):
-        r_a = -1 / T[a] * math.log(alpha / (m * Cnk(T[a] + d - 1, d - 1)))
+        if ra_choice == 'classic_cnk':
+            r_a = count_classic_cnk_ra(T[a], alpha, d, m)
+        elif ra_choice == 'Mardia':
+            r_a = count_mardia_ra(T[a], alpha, d, m)
+        elif ra_choice == 'Agrawal':
+            r_a = count_agrawal_ra(T[a], alpha, d, m)
 
         # min_value = solve_cvx_dual_individual(q_hat[a], r_a)
         min_value, alpha_a_primal = solve_cvx_primal_individual(q_hat[a], r_a, d)
@@ -132,15 +204,17 @@ def get_q_distribution(c_hat, d):
 def run_graph(g, edges_num_dict, args, start_node, finish_node, verbose=False):
     m = len(g.edges)
     if args.mode == 'binomial_with_binomial_T':
-        c_hat, c_bar, T, p = distribution_utils.create_binomial_costs_with_binomial_T(m, d=args.d, T_min=args.T_min,
+        c_hat, c_bar, T, _ = distribution_utils.create_binomial_costs_with_binomial_T(m, d=args.d, T_min=args.T_min,
                                                                                       T_max=args.T_max, verbose=verbose)
     elif args.mode == 'multinomial':
-        c_hat, c_bar, T, p = distribution_utils.create_multinomial_costs(m, d=args.d, T_min=args.T_min,
+        c_hat, c_bar, T, _ = distribution_utils.create_multinomial_costs(m, d=args.d, T_min=args.T_min,
                                                                          T_max=args.T_max, verbose=verbose)
     elif args.mode == 'binomial':
-        c_hat, c_bar, T, p = distribution_utils.create_binomial_costs(m, d=args.d, T_min=args.T_min,
+        c_hat, c_bar, T, _ = distribution_utils.create_binomial_costs(m, d=args.d, T_min=args.T_min,
                                                                       T_max=args.T_max, verbose=verbose)
-
+    elif args.mode == 'normal':
+        c_hat, c_bar, T, _ = distribution_utils.create_normal_costs(m, d=args.d, T_min=args.T_min,
+                                                                      T_max=args.T_max, std=args.normal_std, verbose=verbose)
     # Nominal
     nominal_expected_loss, path_c_bar = graph_utils.solve_shortest_path(c_bar.astype(float), edges_num_dict, g,
                                                                         start_node, finish_node,
@@ -153,7 +227,7 @@ def run_graph(g, edges_num_dict, args, start_node, finish_node, verbose=False):
 
     # DRO
     q_hat = get_q_distribution(c_hat, args.d)
-    c_worst_dro = get_c_worst_DRO(q_hat, args.alpha, T)
+    c_worst_dro = get_c_worst_DRO(q_hat, args.alpha, T, ra_choice=args.ra_choice)
     _, path_c_worst_dro = graph_utils.solve_shortest_path(c_worst_dro.astype(float), edges_num_dict, g, start_node,
                                                           finish_node, verbose=False)
     expected_loss_dro = np.sum(path_c_worst_dro * c_bar)
@@ -174,10 +248,13 @@ def parse_args():
     parser.add_argument('--d', type=int, default=10, help='num of different possible weights values')
     parser.add_argument('--T_min', type=int, default=10, help='min samples num')
     parser.add_argument('--T_max', type=int, default=100, help='max samples num')
-    parser.add_argument('--alpha', type=int, default=0.05, help='feasible error')
+    parser.add_argument('--alpha', type=int, default=1e-6, help='feasible error')
+    parser.add_argument('--ra_choice', type=str, default='classic_cnk', help='choose the radius calculation algorithm',
+                        choices=['classic_cnk', 'Agrawal', 'Mardia'])
+    parser.add_argument('--normal_std', type=int, default=5, help='std for normal data distribution')
     parser.add_argument('--num_exps', type=int, default=10, help='number of runs with different distributions')
     parser.add_argument('--mode', type=str, default='binomial', help='number of runs with different distributions',
-                        choices=['binomial_with_binomial_T', 'multinomial', 'binomial'])
+                        choices=['binomial_with_binomial_T', 'multinomial', 'binomial', 'normal'])
     args = parser.parse_args()
     return args
 
